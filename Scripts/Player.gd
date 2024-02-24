dfc269ab699479bc626eaf8efdb00ef55810369c
Mathew <mathew.tran.dev@gmail.com>
2024-02-23 16:49:54 -0800

    add player controller and dialogue

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,12 @@
-extends Node
+extends CharacterBody2D
 
+var speed = 40
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
+func get_input():
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = input_dir * speed
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	get_input()
+	var collision = move_and_collide(velocity * delta)
