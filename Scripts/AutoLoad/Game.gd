a22e4618655830c8e7aa829e77b87edae36d1147
Mathew <mathew.tran.dev@gmail.com>
2024-02-27 09:09:38 -0800

    update palletes for title

--- Scripts/AutoLoad/Game.gd
+++ Scripts/AutoLoad/Game.gd
@@ -9,6 +9,26 @@ var GridSize = Vector2(10, 10)
 signal DataUpdate
 signal WorkUpdate
 
+
+var ColorData = {
+	"brown" : "987b4e",
+	"blue" : "4896cd",
+	"grey" : "585b65",
+	"purple" : "734f99",
+	"red": "bb3330",
+	"darkbrown" : "5f372c",
+	"pink" : "e7ccb4",
+	"orange" : "b67b2e",
+	"white" : "fcfaf6",
+	"black" : "13110d",
+	"lightblue": "9aa5bf",
+	"navyblue" : "142c4b",
+	"tourquise" : "426773",
+	"silver" : "91877b",
+	"palegreen" : "aab69a",
+	"darkgreen" : "474f2d"
+}
+
 var Data = {
 }
 
